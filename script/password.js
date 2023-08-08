5095d33dcca4926c8332b8ff84f6b4399e231dcc
Alexander Letov <74877926+qnezor@users.noreply.github.com>
2023-08-08 23:11:11 +0300

    Update password.js

--- script/password.js
+++ script/password.js
@@ -6,8 +6,8 @@ function generate() {
         event2_subname.textContent = "У Вас не получиться перехитрить систему...";
         event2_button.textContent = "Принять судьбу";
     } else {
-        document.getElementById('event2').style.zIndex = "3";
-        document.getElementById('event2').style.opacity = "1";
+        event2.style.zIndex = "3";
+        event2.style.opacity = "1";
         event2_name.textContent = "ваш пароль:";
         if (mon < 10 && day < 10 && health < 100 && health >= 10) {
             var aboba = "0" + health + "-" + year + "Y" + "0" + day + "-" + "0" + mon + "C" + "-" + (money * 2);
